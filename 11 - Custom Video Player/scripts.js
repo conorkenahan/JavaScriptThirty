3e3562ae0d08d3f0449a4bae267afd54c8beafaa
Conor <67439130+conorkenahan@users.noreply.github.com>
2021-03-08 14:59:43 -0500

    pause/play added

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,12 +4,19 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.toggle');
-
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelector('[data-skip');
 const ranges = player.querySelector('.player__slider');
 
-
 /* build functions */ 
+
+function togglePLay() {
+    if(video.paused) {
+        video.play();
+    }
+    else {
+        video.pause();
+    }
+}
 
 /* hook up event listeners */ 
